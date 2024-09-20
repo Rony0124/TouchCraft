cec877524df3e75da80c136e93eda518415ce414
rony <siamore9724@gmail.com>
2024-09-20 13:35:17 +0900

    캐릭터 움직임 개선

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,45 +1,73 @@
+using Cinemachine.Utility;
 using PlayerInput;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float velocity = 1f;
-    
-    private CharacterController characterController;
-    private bool isMoving = false;
-    
-    public bool IsOngoing => isMoving;
-    
-    private void Awake() {
-        characterController = GetComponent<CharacterController>();
-    }
-
-    private void Update()
+    public class PlayerMovement : MonoBehaviour
     {
-        Move();
-    }
+        [SerializeField] private float velocity = 1f;
+        [SerializeField] private float rotateSpeed = 1f;
     
-    private void Move()
-    {
-        var axis = PlayerInputHandler.Instance.localInputInfo.InputAxis;
-        if (axis != Vector2.zero)
-        {
-            var direction = Vector3.right * axis.x + Vector3.forward * axis.y;
-            Execute(direction);
-            return;
+        [SerializeField] private Vector3 verticalAxis;
+        [SerializeField] private Vector3 horizontalAxis;
+    
+        private CharacterController characterController;
+        private bool isMoving = false;
+    
+        private Vector3 upAxis => Vector3.Cross(verticalAxis, horizontalAxis).normalized;
+    
+        public bool IsOngoing => isMoving;
+    
+        private void Awake() {
+            characterController = GetComponent<CharacterController>();
         }
 
-        Cancel();
-    }
+        private void Update()
+        {
+            Move();
+        }
 
-    private void Execute(Vector3 direction) {
-        transform.localRotation = Quaternion.LookRotation(direction);
-        characterController.Move(transform.forward * (velocity * Time.deltaTime));
+        private void Move()
+        {
+            var axis = PlayerInputHandler.Instance.localInputInfo.InputAxis;
+            if (axis != Vector2.zero)
+            {
+                var direction = Vector3.right * axis.x + Vector3.forward * axis.y;
+            
+                Execute(direction);
+                return;
+            }
+
+            Cancel();
+        }
+
+        private void Execute(Vector3 direction) {
+            SetPlayerRotation(direction);
+            characterController.Move(transform.forward * (velocity * Time.deltaTime));
         
-        isMoving = true;
-    }
+            isMoving = true;
+        }
+    
+        void SetPlayerRotation(Vector3 dir)
+        {
+            var orientation = dir;
+            orientation.y = 0.0f;
 
-    private void Cancel() {
-        isMoving = false;
+            if (orientation.AlmostZero())
+                return;
+        
+            var tr = transform;
+            var normalizedOrientation = orientation.normalized;
+
+            var current = tr.rotation;
+            var target = Quaternion.LookRotation(normalizedOrientation, upAxis);
+
+            tr.rotation = Quaternion.RotateTowards(current, target, rotateSpeed * Time.deltaTime);
+        }
+
+        private void Cancel() {
+            isMoving = false;
+        }
     }
 }
